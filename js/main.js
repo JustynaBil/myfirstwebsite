20af5bb5e879be3ca0fd8baea83367d8472dc0e5
JustynaBil <bolesja@ukr.net>
2016-04-07 20:39:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ $(document).ready(function(){
     interval: 5000
   });
 });
+$('#btn').click(function() {
+  $.ajax({
+      url: "https://formspree.io/bolesja@ukr.net",  
+      method: "POST",
+      data: {
+             name: name.value,
+             email: email.value,
+             message: message.value
+             }, 
+      dataType: "json"
+  }).done(function() {
+     $('#submit').html('<h1>Thanks for attention</h1>')
+  } );
+});
